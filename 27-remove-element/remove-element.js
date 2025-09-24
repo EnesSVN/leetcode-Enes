d0bdd21218bded8d661122cc8089454ba56c3b9c
Enes SEVEN <79689056+EnesSVN@users.noreply.github.com>
2025-09-24 11:55:28 +0300

    Time: 0 ms (100.00%) | Memory: 54.6 MB (14.33%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let writeIndex = 0;
+  for (let readIndex = 0; readIndex < nums.length; readIndex++) {
+    if (nums[readIndex] !== val) {
+      nums[writeIndex] = nums[readIndex];
+      writeIndex++;
+    }
+  }
+   return writeIndex;
+};
